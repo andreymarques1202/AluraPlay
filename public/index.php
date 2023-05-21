01c643fd6ed9af806d3f7f0beac83be7acace30c
Andrey Marques <andreymarques822@gmail.com>
2023-05-21 20:09:12 -0300

    Backend: Adicionando rotas

--- public/index.php
+++ public/index.php
@@ -10,27 +10,22 @@ use Alura\Mvc\Controller\{
     $dbPath = __DIR__ . '/../banco.sqlite';
     $pdo = new PDO("sqlite:$dbPath");
     $videoRepository = new VideoRepository($pdo);
+    
+    $routes = require_once "../config/routes.php";
+
+    $pathInfo = $_SERVER["PATH_INFO"] ?? "/";
+    $httpMethod = $_SERVER["REQUEST_METHOD"];
+
+    $key = "$httpMethod|$pathInfo";
+
+
+    if (array_key_exists($key, $routes)) {
         
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+    $controller = new $controllerClass($videoRepository);
 
-      if (!array_key_exists("PATH_INFO", $_SERVER) || $_SERVER["PATH_INFO"] === "/") {
-        $controller = new VideoListController($videoRepository);
-       } elseif ($_SERVER["PATH_INFO"] === "/novo-video") {
-        if ($_SERVER["REQUEST_METHOD"] === "GET") {
-            $controller = new VideoFormController($videoRepository);
-        } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $controller = new NewVideoController($videoRepository);
-        }
-       } elseif ($_SERVER["PATH_INFO"] === "/editar-video") {
-            if ($_SERVER["REQUEST_METHOD"] === "GET") {
-                $controller = new VideoFormController($videoRepository);
-            } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $controller = new EditVideoController($videoRepository);
-            }
-       } elseif ($_SERVER["PATH_INFO"] === "/remover-video") {
-        $controller = new DeleteVideoController($videoRepository);
-       } else {
+    } else {
         $controller = new Error404Controller();
-       }
-
+    }
        $controller->processaRequisicao();
 ?>
